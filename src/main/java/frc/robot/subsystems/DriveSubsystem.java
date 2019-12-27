9662fa45742aee39de6b150e129af33f46409cbc
Nelson Green <nelson.green@GMAIL.COM>
2019-12-27 14:25:00 -0600

    Added more to ramsete drivesubsystem

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -53,23 +53,78 @@ public class DriveSubsystem extends SubsystemBase {
 
   private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
 
-    // The left-side drive encoder
-    private final Encoder m_leftEncoder =
-    new Encoder(kLeftEncoderPorts[0], kLeftEncoderPorts[1], kLeftEncoderReversed);
+  // The left-side drive encoder
+  private final Encoder m_leftEncoder =
+  new Encoder(kLeftEncoderPorts[0], kLeftEncoderPorts[1], kLeftEncoderReversed);
 
-    // The right-side drive encoder
-    private final Encoder m_rightEncoder =
-    new Encoder(kRightEncoderPorts[0], kRightEncoderPorts[1], kRightEncoderReversed);
+  // The right-side drive encoder
+  private final Encoder m_rightEncoder =
+  new Encoder(kRightEncoderPorts[0], kRightEncoderPorts[1], kRightEncoderReversed);
 
-public DriveSubsystem(){
-  m_leftEncoder.setDistancePerPulse(kEncoderDistancePerPulse);
-  m_rightEncoder.setDistancePerPulse(kEncoderDistancePerPulse);
+  // The gyro sensor
+  private final Gyro m_gyro = new ADIS16448_IMU();
 
-}
+  // Odometry class for tracking robot pose
+  private final DifferentialDriveOdometry m_odometry;
 
-public void arcadeDrive(double fwd, double rot){
-  m_drive.arcadeDrive(-fwd, rot);
-}
+  /**
+   * Creates a new DriveSubsystem
+   */
+  public DriveSubsystem(){
+    // Sets the distance per pulse for the encoders
+    m_leftEncoder.setDistancePerPulse(kEncoderDistancePerPulse);
+    m_rightEncoder.setDistancePerPulse(kEncoderDistancePerPulse);
+
+    resetEncoders();
+    m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getHeading()));
+  }
+
+  @Override
+  public void periodic() {
+    // Update the odometry in the periodic block
+    m_odometry.update(Rotation2d.fromDegrees(getHeading()), m_leftEncoder.getDistance(),
+        m_rightEncoder.getDistance());
+  }
+
+  /**
+   * Returens the currently-estimated pose of the robot.
+   * 
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+  }
+
+  /**
+   * Returns the current wheel speeds of the robot.
+   * 
+   * @return The current wheel speeds
+   */
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(),m_rightEncoder.getRate());
+  }
+
+  /** 
+   * Resets the odometry to the specified pose.
+   * 
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    m_odometry.resetPosition(pose, Rotation2d.fromDegrees(getHeading()));
+  }
+  
+  /**
+   * Drives the robot using arcade controls.
+   *  
+   * @param fwd the commanded forward movement
+   * @param rot the commanded rotation
+   */
+  public void arcadeDrive(double fwd, double rot){
+    m_drive.arcadeDrive(fwd, rot);
+  }
+
+
 
   /**
    * Resets the drive encoders to currently read a position of 0.
