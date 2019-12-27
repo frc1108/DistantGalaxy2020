0604873f6e009ecb1000f792fd4c602c72d6188c
Nelson Green <nelson.green@GMAIL.COM>
2019-12-27 14:35:55 -0600

    Updated ramsete drive subsystem

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -124,7 +124,16 @@ public class DriveSubsystem extends SubsystemBase {
     m_drive.arcadeDrive(fwd, rot);
   }
 
-
+  /**
+   * Controls the left and right sides of the drive directly with voltages.
+   * 
+   * @param leftVolts the commanded left output
+   * @param rightVolts the commanded right output
+   */
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+    m_leftMotors.setVoltage(leftVolts)
+    m_rightMotors.setVoltage(-rightVolts);
+  }
 
   /**
    * Resets the drive encoders to currently read a position of 0.
@@ -160,8 +169,38 @@ public class DriveSubsystem extends SubsystemBase {
   public Encoder getRightEncoder() {
     return m_rightEncoder;
   }
-  
-public void setMaxOutput(double maxOutput){
-  m_drive.setMaxOutput(maxOutput);
-}
+
+  /**
+   * Sets the max output of the drive. Useful for scaling the drive to drive more slowly.
+   * 
+   * @param maxOutput the maximum output to which the drive will be constrained
+   */
+  public void setMaxOutput(double maxOutput){
+    m_drive.setMaxOutput(maxOutput);
+  }
+
+  /**
+   * Zeroes the heading of the robot
+   */
+  public void zeroHeading() {
+    m_gyro.reset();
+  }
+
+  /**
+   * Returns the heading of the robot.
+   * 
+   * @return the robot's headin in degrees, from -180 to 180.
+   */
+  public double getHeading() {
+    return Math.IEEEremainder(m_gyro.getAngle(), 360)*(kGyroReversed?-1.0:1.0);
+  }
+
+  /**
+   * Returns the turn rate of the robot.
+   * 
+   * @return The turn rate of the robot, in degrees per second
+   */
+  public double getTurnRate() {
+    return m_gyro.getRate()*(kGyroReversed?-1.0:1.0);
+  }
 }
